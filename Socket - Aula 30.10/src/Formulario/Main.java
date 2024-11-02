4e1fed997315fc567706bf4bcc75391a6c658ccc
laboratorio <laboratorio@LAB10DT05.ACAD.UFN.EDU.BR>
2024-11-01 21:16:53 -0300

    Documentação dos códigos de thread e socke

--- Socket - Aula 30.10/src/Formulario/Main.java
+++ Socket - Aula 30.10/src/Formulario/Main.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Formulario;
 
 import DAO.PessoaDAO;
@@ -14,34 +10,43 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- *
+ * Cria um servidor TCP que aceia conexões de clientes e utiliza threads para
+ * gerenciar cada conexão individualmente
+ * 
+ * O servidor escuta uma porta específica e, ao receber conexão,
+ * cria uma nova thread através da classe ThreadServer para lidar com
+ * solicitações do cliente
  * @author laboratorio
  */
 public class Main {
     public static void main(String[] args) throws IOException {
         int porta = 12345; // Use uma constante para a porta
-        
-        try (ServerSocket servidorSocket = new ServerSocket (porta)){
+        //ServerSocket permite que o servidor aceite conexões na porta especificada.
+        try (ServerSocket servidorSocket = new ServerSocket (porta)){ 
             System.out.println("Servidor aguardando conexoes na porta "+porta);
 
             while(true){
                 try{
+                    // O servidor bloqueia até que um cliente se conecte. 
+                    //Uma vez conectado, um novo Socket é criado para se comunicar com o cliente.
                     Socket clienteSocket = servidorSocket.accept();
+                    
+                    // mostra o endereço do cliente que se conectou
                     System.out.println("Conexao aceita de "+ clienteSocket.getInetAddress());
+                    
                     Thread threadCliente = new ThreadServer(clienteSocket);
+                    //inicia a thread que executa o metodo run(), permite que o servidor continue aceitando novas conexões
+                    //enquanto a thread processa a conexão atual.
                     threadCliente.start();
                     
                     /*
                     ObjectOutputStream out = new ObjectOutputStream (clienteSocket.getOutputStream());
                     ObjectInputStream in = new ObjectInputStream (clienteSocket.getInputStream());
-
                     int id = in.readInt();
                     System.out.println("Id recebido: "+ id);
-
                     //simule a obtencao de uma pessoa a partir do id
                     PessoaDAO pdao = new PessoaDAO();
                     Pessoa p = pdao.getPessoa(id);
-
                     out.writeObject(p);
                     */
                     
--- Socket - Aula 30.10/src/Thread/ThreadServer.java
+++ Socket - Aula 30.10/src/Thread/ThreadServer.java
@@ -12,30 +12,44 @@ import java.io.ObjectOutputStream;
 import java.net.Socket;
 
 /**
- *
+ * Classe que gerencia a comunicação com um cliente conectado ao servidor. 
+ * Cada instância é executada em uma thread separada,
+ * permitindo atender múltiplos clientes simultaneamente
  * @author laboratorio
  */
 public class ThreadServer extends Thread {
     private Socket clienteSocket;
     
-    public ThreadServer (Socket clienteSocket){
+    /**
+     * Construtor da Classe ThreadServer
+     * O construtor recebe um objeto Socket que representa a conexão com o cliente. 
+     * Este socket é usado para enviar e receber dados.
+     * @param clienteSocket 
+     */
+    
+    public ThreadServer (Socket clienteSocket){ //cria o construtor
         this.clienteSocket = clienteSocket;
     }
-
+    /**
+     * Método executado quando a thread é inciada.
+     * Gerencia a comunicação com cliente, de entrada (in) e saida (out).
+     */
     @Override
-    public void run() {
+    public void run() { 
         try {
             ObjectOutputStream out = new ObjectOutputStream (clienteSocket.getOutputStream());
+            //fluxo de saida para enviar objetos ao ciente
             ObjectInputStream in = new ObjectInputStream (clienteSocket.getInputStream());
-            
-            int id = in.readInt();
+            //fluxo de entrada para receber objetos do cliente
+           
+            int id = in.readInt(); //le um inteiro enviado pelo cliente
             System.out.println("Id Recebido: "+id);
             
             //simule a obtencao de uma Pessoa a partir do ID
             PessoaDAO pdao = new PessoaDAO();
             Pessoa p = pdao.getPessoa(id);
             
-            out.writeObject(p);
+            out.writeObject(p); //envia o objeto Pessoa encontrado de volta ao cliente 
             
         }catch(IOException e){
             System.out.println("Erro ao lidar com o cliente");
